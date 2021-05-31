8e95fd5733b10f2b19cf2508ea9ae84cec511517
irondi <irondicjonathan@gmail.com>
2021-05-31 19:35:26 +0100

    added connection links

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,27 @@
 import "../styles/header.css";
 import { ReactComponent as Arrow } from "../icons/arrow-down.svg";
-
+import { ReactComponent as Github } from "../icons/github.svg";
+import { ReactComponent as Twitter } from "../icons/twitter.svg";
+import { ReactComponent as Instagram } from "../icons/instagram.svg";
+import { ReactComponent as Email } from "../icons/email.svg";
 function header() {
   return (
     <header>
       <div className="content">
+        <div className = "profilelinks">
+          <div className="links">
+            <Github height={30} width={30} />
+          </div>
+          <div className="links">
+            <Twitter height={30} width={30}/>
+          </div>
+          <div className="links">
+          <Instagram height={30} width={30}/>
+          </div>
+          <div className="links">
+          <Email height={30} width={30}/>
+          </div>
+        </div>
         <div>
           <h1>Jonathan Irondi</h1>
           <h3>I love everything about code</h3>
